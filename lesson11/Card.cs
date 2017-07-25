56d9f5817dd383102a0437274dae8d998f5b014b
Anupom Roy <aroy34@my.centennialcollege.ca>
2017-07-25 15:52:02 -0400

    Created Deck Class

--- lesson11/Card.cs
+++ lesson11/Card.cs
@@ -7,5 +7,51 @@ namespace lesson11
 {
     public class Card
     {
+        private Faces _face;
+        private Suit _suit;
+
+        public Faces Faces
+        {
+            get
+            {
+                return this._face;
+            }
+
+            set
+            {
+                this._face = value;
+            }
+        }
+        public Suit Suit
+        {
+            get
+            {
+                return this._suit;
+            }
+
+            set
+            {
+                this._suit = value;
+            }
+        }
+
+        // CONSTRUCTORS
+
+        /// <summary>
+        /// This is the main constructor for the Card class.
+        /// It takes two parameters - face (Face) - suit (Suit)
+        /// </summary>
+        /// <param name="face"></param>
+        /// <param name="suit"></param>
+        public Card(Faces face, Suit suit)
+        {
+            this.Faces = face;
+            this.Suit = suit;
+        }
+
+
+        // PRIVATE METHODS
+
+        // PUBLIC METHODS
     }
 }
---
+++ lesson11/Deck.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+/*
+Name: Anupom
+date: july 25
+ Description: 
+ * Version: 0.1 - Created the Deck class
+ */
+namespace lesson11
+{
+    public class Deck:List<Card>
+    {
+
+        public Deck()
+        {
+            this._initialize();
+        }
+        private void _initialize()
+        {
+            for (int suit = 0; suit < (int)Suit.Spreads; suit++)
+            {
+                for (int faces = 0; faces < (int )Faces.King; faces++)
+                {
+                    this.Add(new Card((Faces)faces, (Suit)suit));
+                }
+            }
+
+            
+        }
+        public override string ToString()
+        {
+            string outputstring = "";
+            foreach (Card card in this)
+            {
+                outputstring += " The" + card.Faces + "of" + card.Suit+"\n";
+            }
+            return outputstring;
+        }
+    }
+}
